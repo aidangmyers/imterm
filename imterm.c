a1309886fadad3b4ee8f754a069dbd5237a35ac5
aidangmyers <aidangmyers@gmail.com>
2024-02-10 14:34:47 -0500

    first messages window, artifacts with resizing and init on messages window

--- imterm.c
+++ imterm.c
@@ -8,7 +8,7 @@
 
 // MACROS
 #include <curses.h>
-#define REFRESH_RATE 500      // in miliseconds
+#define REFRESH_RATE 1000      // in miliseconds
 
 // libraries
 #include "nwrapper.c"
@@ -31,13 +31,24 @@ int main() {
   int local_padding_y = 2 * global_padding_y;
 
   // chats WINDOW*
-  struct window* chats = construct_window(global_padding_x, global_padding_y, (int)(max_x / 5), (int)(max_y - local_padding_y), 1);
+  int chats_pos_x = global_padding_x;
+  int chats_pos_y = global_padding_y;
+  int chats_size_x = (int)((max_x - 3 * global_padding_x ) * .2);
+  int chats_size_y = (int)(max_y - local_padding_y);
+  struct window* chats = construct_window(chats_pos_x, chats_pos_y, chats_size_x, chats_size_y, 1);
 
-  struct window messages;
+  // messages WINDOW*
+  int messages_pos_x = 2 * global_padding_x + chats_size_x;
+  int messages_pos_y = global_padding_y;
+  int messages_size_x = (int)((max_x - 3 * global_padding_x ) * .8);
+  int messages_size_y = (int)(max_y - local_padding_y);
+  struct window* messages = construct_window(messages_pos_x, messages_pos_y, messages_size_x, messages_size_y, 1);
 
   // create color pair
   start_color();
   init_pair(1, COLOR_WHITE, COLOR_BLACK);
+
+  curs_set(0);
 
   // === primary loop ===
   do {
@@ -47,19 +58,31 @@ int main() {
     getmaxyx(stdscr, max_y, max_x);
 
     // update windows
+    chats_pos_x = global_padding_x;
+    chats_pos_y = global_padding_y;
+    chats_size_x = (int)((max_x - 3 * global_padding_x ) * .2);
+    chats_size_y = (int)(max_y - local_padding_y);
     box(chats->pointer, 0, 0);
-    update_window(chats, global_padding_x, global_padding_y, (int)(max_x / 5), max_y - local_padding_y, 1);
+    update_window(chats, chats_pos_x, chats_pos_y, chats_size_x, chats_size_y, 1);
+
+    messages_pos_x = 2 * global_padding_x + chats_size_x;
+    messages_pos_y = global_padding_y;
+    messages_size_x = (int)((max_x - 3 * global_padding_x ) * .8);
+    messages_size_y = (int)(max_y - local_padding_y);
+    box(messages->pointer, 0, 0);
+    update_window(messages, messages_pos_x, messages_pos_y, messages_size_x, messages_size_y, 1);  
 
     // create text
     mvwaddnstr(chats->pointer, 1, 1, "hello color!", -1);
 
     // global updates
-    move(0, 0);   // to move the cursor
     refresh();
     usleep(REFRESH_RATE);
-    refresh();
 
   } while ((char)getch() != 'q'); // will have to be changed when a q is typed lol
+
+  free(chats);
+  free(messages);
 
   endwin();
 
